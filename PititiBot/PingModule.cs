83efddddcc70d40721170c0c313ed97a17eda397
ReboundQ3 <22770594+ReboundQ3@users.noreply.github.com>
2025-12-02 21:43:55 +0100

    Remove the lizard from the pingmodule

--- PititiBot/PingModule.cs
+++ PititiBot/PingModule.cs
@@ -6,6 +6,6 @@ public class PingModule : InteractionModuleBase<SocketInteractionContext>
     [SlashCommand("ping", "Checks if Pititi is alive")]
     public async Task HandlePingCommand()
     {
-        await RespondAsync("HIHI IS OF PITITI!! 🦎");
+        await RespondAsync("HIHI IS OF PITITI!!");
     }
 }
