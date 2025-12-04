18621c842f29162b3d40d14aa9743dcf9b8c99c7
ReboundQ3 <22770594+ReboundQ3@users.noreply.github.com>
2025-12-04 16:10:54 +0100

    Testing the new help command

--- PititiBot/Modules/HelpModule.cs
+++ PititiBot/Modules/HelpModule.cs
@@ -12,19 +12,28 @@ public class HelpModule : InteractionModuleBase<SocketInteractionContext>
     private readonly DiscordSocketClient _client;
     private readonly InteractionService _interactionService;
 
-    [SlashCommand("help", "Shows all commands")]
+    public HelpModule(DiscordSocketClient client, InteractionService interactionService)
+    {
+        _client = client;
+        _interactionService = interactionService;
+    }
 
+    [SlashCommand("help", "Shows all commands")]
     public async Task HandleServerCommand()
     {
+        
+    }
+
+    public async Task ConvertToEmbed()
+    {
         var embed = new EmbedBuilder()
-        {
-            
-        };
+            .WithTitle("Pititi list of HELPINGS!!")
+            .WithDescription("These are the commands Pititi knows");
 
         var slashCommands = _interactionService.SlashCommands.ToList();
         foreach (var slashCommand in slashCommands)
         {
-            await RespondAsync($"");
+            embed.AddField($"{slashCommand.Name}",slashCommand.Description);
         }
     }
 }
